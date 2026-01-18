94cd7c9f71c1f671d1b9259361075c4c6bd0c422
Wasilni <mhmdalrmymh183@gmail.com>
2026-01-19 02:32:14 +0300

    Update kw.py

--- kw.py
+++ kw.py
@@ -1847,3 +1847,5 @@ def main():
     # 3. بدء التشغيل
     print("🚀 البوت يعمل الآن بنجاح وبترتيب صحيح...")
     application.run_polling(drop_pending_updates=True)
+if __name__ == '__main__':
+    main()
